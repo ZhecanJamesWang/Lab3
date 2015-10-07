e420e4a31dccfbd8dca542b44e1abdba0b3aa19a
Jordan Van Duyne <jordannn808@gmail.com>
2015-10-06 20:00:36 -0400

    start connection to db

---
+++ ScavengerHunt/app/src/main/java/com/mobileproto/jovanduy/scavengerhunt/Database.java
@@ -0,0 +1,77 @@
+package com.mobileproto.jovanduy.scavengerhunt;
+
+import android.util.Log;
+
+import java.sql.*;
+import java.math.*;
+
+/**
+ * Created by Jordan on 10/6/15.
+ */
+public class Database {
+
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://45.55.65.113/mobproto";
+
+    // database credentials
+    static final String USER = "student";
+    static final String PASS = "MobProto";
+    static final String KEY_ID = "AKIAISEFKD6O3QSZGHUQ";
+    static final String SECRET_KEY = "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";
+
+    public void ConnectToDb() {
+        Connection conn = null;
+        Statement stmt = null;
+
+        try {
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 3: Open a connection
+            Log.d("Connecting", "Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+//
+//            //STEP 4: Execute a query
+//            Log.d("Database","Creating statement...");
+//            stmt = conn.createStatement();
+//            String sql = "SELECT id, first, last, age FROM Employees";
+//            ResultSet rs = stmt.executeQuery(sql);
+//
+//            //STEP 5: Extract data from result set
+//            while(rs.next()) {
+//                //Retrieve by column name
+//                int id  = rs.getInt("id");
+//                int age = rs.getInt("age");
+//                String first = rs.getString("first");
+//                String last = rs.getString("last");
+//
+//                //Display values
+//                System.out.print("ID: " + id);
+//                System.out.print(", Age: " + age);
+//                System.out.print(", First: " + first);
+//                System.out.println(", Last: " + last);
+//            }
+//            //STEP 6: Clean-up environment
+//            rs.close();
+//            stmt.close();
+            conn.close();
+
+        } catch(SQLException se) {
+            se.printStackTrace();
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    stmt.close();
+            } catch(SQLException se2) { }
+            try {
+                if(conn!=null)
+                    conn.close();
+            } catch(SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ ScavengerHunt/app/src/main/java/com/mobileproto/jovanduy/scavengerhunt/VideoFragment.java
@@ -0,0 +1,22 @@
+package com.mobileproto.jovanduy.scavengerhunt;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Jordan on 10/6/15.
+ */
+public class VideoFragment extends Fragment {
+
+    public VideoFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
