db8edfad72264d9dd552dc70b696f74705ce65eb
Jordan Van Duyne <jordannn808@gmail.com>
2015-10-22 19:30:16 -0400

    add HuntProgress

---
+++ ScavengerHunt/app/src/main/java/com/mobileproto/jovanduy/scavengerhunt/HuntProgress.java
@@ -0,0 +1,97 @@
+package com.mobileproto.jovanduy.scavengerhunt;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+
+/**
+ * Class for keeping track of the stage of the scavenger hunt
+ */
+public class HuntProgress { //TODO: actually use this class! Good for MVC
+
+    private int currStage;
+    private int stageFinal;
+    private ArrayList<Double> latitudes;
+    private ArrayList<Double> longitudes;
+    private ArrayList<String> videos;
+    private ArrayList<String> images;
+    private String urlBase = "https://s3.amazonaws.com/olin-mobile-proto/";
+    private boolean onLastStage;
+
+
+
+
+//    private int stage = 0;
+//    private ArrayList<Double> latitudes;
+//    private ArrayList<Double> longitudes;
+//    private ArrayList<String> videos;
+//    private String urlBase = "https://s3.amazonaws.com/olin-mobile-proto/";
+//    private ArrayList<String> images;
+    private boolean pathGotten;
+
+    public HuntProgress(Context context) {
+        pathGotten = false;
+        onLastStage = false;
+    }
+
+    public boolean foundPath() {
+        return pathGotten;
+    }
+
+    public boolean isOnLastStage() {
+        return onLastStage;
+    }
+
+    public void setOnLastStage(boolean bool) {
+        onLastStage = bool;
+    }
+
+    public int getCurrStage() {
+        return currStage;
+    }
+
+    public void updateCurrStage(int stage) {
+        currStage = stage;
+    }
+
+    public int getStageFinal() {
+        return stageFinal;
+    }
+
+    public void updateStageFinal(int stage) {
+        stageFinal = stage;
+    }
+
+    public String getUrl(int stage) {
+        return urlBase + videos.get(stage);
+    }
+
+    public void addUrl(String url) {
+        videos.add(url);
+    }
+
+    public double getLatitude(int stage) {
+        return latitudes.get(stage);
+    }
+
+    public double getLongitude(int stage) {
+        return longitudes.get(stage);
+    }
+
+    public void addLatitude(double latitude) {
+        latitudes.add(latitude);
+    }
+
+    public void addLongitude(double longitude) {
+        longitudes.add(longitude);
+    }
+
+    public void setImage(String image) {
+//        images.add(image)
+        images.set(currStage, image);
+    }
+
+    public String getImage(int stage) {
+        return images.get(stage);
+    }
+}
